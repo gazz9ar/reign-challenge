11583fb78697b4ef6181a8528036f041b7a3a70a
Gaspar Santillan <69467104+gazz9ar@users.noreply.github.com>
2022-07-21 22:46:31 -0300

    maquetado

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface categoriesFilter {
+	id:number,
+	name:string,
+	fileName:string,
+	like:boolean
+}
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
+	public categories:categoriesFilter[] = [
+		{id:0, name:"Angular",fileName:"angular",like:true},
+		{id:1, name:"Reactjs",fileName:"reactjs",like:false},
+		{id:2, name:"Vuejs",fileName:"vuejs",like:true}
+	]
+	public showCategories:boolean = false;
+	public selected:any = "Select your news";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public showList(): void {	
+	if (this.showCategories) {
+		this.showCategories = false;
+	} else {
+		this.showCategories = true;
+	}	
+  }
+  public changeSelected(categorie:string): void {		
+	this.selected = categorie;
+	this.showCategories = false;
+  }
+
+  public like() {
+	console.error('LIKEEE')
   }
 
 }
